1a1b364fe5dbbb342a3b481d44a99ce0952d0395
sarahhii <sarah164@hotmail.de>
2021-08-18 13:05:10 +0200

    adjustments
    hovering adjustments, more details in bubChar, animation settings

--- scripts/bubble.js
+++ scripts/bubble.js
@@ -15,7 +15,8 @@ const colors = {
 const generateChart = data => {
     const bubble = data => d3.pack()
         .size([width, height])
-        .padding(2)(d3.hierarchy({
+        //padding between bubbles
+        .padding(4)(d3.hierarchy({
             children: data
         }).sum(d => d.score));
 
@@ -35,18 +36,25 @@ const generateChart = data => {
     const circle = node.append('circle')
         //fill with color depending on category
         .style('fill', d => colors[d.data.category])
-    
-    //hover showing more details
+
+        //hover showing more details
         .on('mouseover', function (e, d) {
             tooltip.select('img').attr('src', d.data.img);
-            tooltip.select('a').attr('href', d.data.link).text(d.data.name);
-            tooltip.select('span').attr('class', d.data.category).text(d.data.category);
-            tooltip.style('visibility', 'visible');
+            tooltip.select('a').attr('class', d.data.category).text(d.data.name);
 
-            d3.select(this).style('stroke', '#222');
+            tooltip.select('span.yearSpan').text(d.data.category);
+                        tooltip.select('span.crashSpan').text(d.data.score);
+            tooltip.select('span.locationSpan').text(d.data.location);
+            tooltip.select('span.countrySpan').text(d.data.country);
+
+            tooltip.style('visibility', 'visible');
+            //stroke when hovering
+            d3.select(this).style('stroke', '#000');
         })
         .on('mousemove', e => tooltip.style('top', `${e.pageY}px`)
             .style('left', `${e.pageX + 10}px`))
+
+        //when mouse moves away, no hover effect anymore
         .on('mouseout', function () {
             d3.select(this).style('stroke', 'none');
             return tooltip.style('visibility', 'hidden');
@@ -57,8 +65,9 @@ const generateChart = data => {
 
     // the definition of the label that is appended to the bubble 
     const label = node.append('text')
-        .attr('dy', 2)
-        .text(d => d.data.name.substring(0, d.r / 3));
+        .attr('dy', 3)
+        //gives padding to circuit name inside of bubble
+        .text(d => d.data.name.substring(0, d.r / 4));
 
     // making sure bubbles are all next to each other and not at the same spot
     node.transition()
@@ -66,30 +75,29 @@ const generateChart = data => {
         .duration(3000)
         .attr('transform', d => `translate(${d.x}, ${d.y})`);
 
-    //
+    //animation 
     circle.transition()
         .ease(d3.easeExpInOut)
-        .duration(3000)
+        .duration(4000)
         .attr('r', d => d.r);
 
     //text inside the bubbles appears a little later
     label.transition()
-        .delay(1500)
+        .delay(2500)
         .ease(d3.easeExpInOut)
         .duration(1000)
         .style('opacity', 1)
         .attr('class', 'classTest')
-    
+
 
 };
-
 
 // starts the graph when the button gets clicked
 document.getElementById("graphBtn").onclick = async () => {
+
     data = await d3.json(file).then(data => data);
+
     generateChart(data);
-    document.getElementById("hidden").style.visibility = "visible"
-    
+    document.getElementById("hidden").style.visibility = "visible";
+
 };
-
-
